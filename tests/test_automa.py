0abc701f9f639af7ee2123a2c3b002f6ec5cb526
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2025-06-06 11:11:15 +0530

    Fix tests

--- tests/test_automa.py
+++ tests/test_automa.py
@@ -2,12 +2,13 @@ from pathlib import Path
 from unittest.mock import patch
 
 import pytest
+from automa.bot import CodeFolder
 from automa.bot.webhook import generate_webhook_signature
 
 from app.env import env
 
 fixtures = Path(__file__).parent / "fixtures"
-fixture_code = fixtures / "code"
+fixture_code = CodeFolder(fixtures / "code")
 
 
 def call_with_fixture(client, filename):
